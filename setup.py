0af21d7935c4131f2e6774f5b1671748e651d2b8
Daniel Wallace <danielwallace@gtmanfred.com>
2018-03-02 22:01:24 -0700

    fix error

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ Test pkg_resources plugin for irc3
 setuptools.setup(
     name='irc3-plugins-test',
     summary='test plugin for irc3',
-    version='0.0.1',
+    version='0.0.2',
     long_description=desc,
     author='Daniel Wallace',
     author_email='daniel@gtmanfred.com',
@@ -20,6 +20,6 @@ setuptools.setup(
     packages=['irc3_plugins_test',],
     entry_points='''
         [irc3.loader]
-        irc3.plugins.test = irc3_plugins_slack.test
+        irc3.plugins.test = irc3_plugins_test.test
     ''',
 )
